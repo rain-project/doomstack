f018ce29fa7e1eeb84d0b94046f10d511e57683d
Matteo Monti <matteo.monti@epfl.ch>
2023-03-21 15:29:02 +0000

    Scaffold `doom` function

---
+++ doomstack_derive/src/doom/mod.rs
@@ -0,0 +1,5 @@
+use proc_macro::TokenStream;
+
+pub(crate) fn doom(_input: TokenStream) -> TokenStream {
+    todo!()
+}
--- doomstack_derive/src/lib.rs
+++ doomstack_derive/src/lib.rs
@@ -1,1 +1,12 @@
+// Modules
+
 mod doom;
+
+// Interface
+
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(Doom, attributes(doom))]
+pub fn doom_derive(input: TokenStream) -> TokenStream {
+    doom::doom(input)
+}
