b63a7dbd7d20a77dcc2496bfa6f3446f9ce57848
Matteo Monti <matteo.monti@epfl.ch>
2023-08-02 17:48:33 +0200

    Wire `Derive::parse` into `doom`

--- doomstack_derive/src/doom/mod.rs
+++ doomstack_derive/src/doom/mod.rs
@@ -24,13 +24,8 @@ use quote::quote;
 use syn::{Data, DeriveInput};
 
 pub(crate) fn doom(input: TokenStream) -> TokenStream {
-    let derive_input: DeriveInput = syn::parse(input).unwrap();
-
-    let Data::Enum(data) = derive_input.data else { todo!() };
-    let variant = &data.variants[0];
-
-    let attributes = variant.attrs.iter().filter_map(Attribute::parse);
-    let _settings = Settings::from_attributes(attributes, variant.ident.span());
+    let input: DeriveInput = syn::parse(input).unwrap();
+    let derive = Derive::parse(&input);
 
     quote!().into()
 }
