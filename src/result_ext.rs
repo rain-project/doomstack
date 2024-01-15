59a776e56cf3e82aa2f6b22a2d890ee4a9e1e6dc
Matteo Monti <matteo.monti@yahoo.it>
2024-01-15 16:00:42 +0100

    Improve order of methods in `ResultExt`

--- src/result_ext.rs
+++ src/result_ext.rs
@@ -11,6 +11,16 @@ pub trait ResultExt<O, E> {
         Self: DoomResult<O>,
         D: Doom;
 
+    fn wrap<W, D>(self, wrap: W) -> Result<O, Top<D>>
+    where
+        W: Fn(E) -> D,
+        D: Doom;
+
+    fn wrap_as_stack<W, D>(self, wrap: W) -> Result<O, Stack>
+    where
+        W: Fn(E) -> D,
+        D: Doom;
+
     fn spot(self, location: Location) -> Self
     where
         Self: DoomResult<O>;
@@ -23,16 +33,6 @@ pub trait ResultExt<O, E> {
     fn pot_as_stack<D>(self, doom: D, location: Location) -> Result<O, Stack>
     where
         Self: DoomResult<O>,
-        D: Doom;
-
-    fn wrap<W, D>(self, wrap: W) -> Result<O, Top<D>>
-    where
-        W: Fn(E) -> D,
-        D: Doom;
-
-    fn wrap_as_stack<W, D>(self, wrap: W) -> Result<O, Stack>
-    where
-        W: Fn(E) -> D,
         D: Doom;
 
     fn wrot<W, D>(self, wrap: W, location: Location) -> Result<O, Top<D>>
@@ -63,6 +63,22 @@ impl<O, E> ResultExt<O, E> for Result<O, E> {
         DoomResult::push_as_stack(self, doom)
     }
 
+    fn wrap<W, D>(self, wrap: W) -> Result<O, Top<D>>
+    where
+        W: Fn(E) -> D,
+        D: Doom,
+    {
+        self.map_err(|error| wrap(error).into_top())
+    }
+
+    fn wrap_as_stack<W, D>(self, wrap: W) -> Result<O, Stack>
+    where
+        W: Fn(E) -> D,
+        D: Doom,
+    {
+        self.map_err(|error| wrap(error).into_stack())
+    }
+
     fn spot(self, location: Location) -> Self
     where
         Self: DoomResult<O>,
@@ -84,22 +100,6 @@ impl<O, E> ResultExt<O, E> for Result<O, E> {
         D: Doom,
     {
         ResultExt::spot(ResultExt::push_as_stack(self, doom), location)
-    }
-
-    fn wrap<W, D>(self, wrap: W) -> Result<O, Top<D>>
-    where
-        W: Fn(E) -> D,
-        D: Doom,
-    {
-        self.map_err(|error| wrap(error).into_top())
-    }
-
-    fn wrap_as_stack<W, D>(self, wrap: W) -> Result<O, Stack>
-    where
-        W: Fn(E) -> D,
-        D: Doom,
-    {
-        self.map_err(|error| wrap(error).into_stack())
     }
 
     fn wrot<W, D>(self, wrap: W, location: Location) -> Result<O, Top<D>>
