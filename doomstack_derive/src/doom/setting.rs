4b11c8e379f06c52eb83284e061efc923ac9078b
Matteo Monti <matteo.monti@yahoo.it>
2024-01-16 22:27:44 +0100

    Document `Setting`

--- doomstack_derive/src/doom/setting.rs
+++ doomstack_derive/src/doom/setting.rs
@@ -1,5 +1,12 @@
 use crate::doom::{Description, KeepOriginal, Wrap};
 
+/// A setting pertaining to the derivation of a group of fields (struct or enum variant).
+///
+/// A [`Setting`] is one of the fields in an [`Attribute`] (which also contains derive-related
+/// information such as [`Spans`]).
+///
+/// [`Attribute`]: crate::doom::Attribute
+/// [`Spans`]: crate::doom::attribute::Spans
 pub(crate) enum Setting {
     Description(Description),
     KeepOriginal(KeepOriginal),
