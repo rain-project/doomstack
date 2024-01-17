0d85c3f49a06e4b0d18fa44d2f9ea7319f03fe60
Matteo Monti <matteo.monti@yahoo.it>
2024-01-17 12:01:53 +0100

    Document `Wrap`

--- doomstack_derive/src/doom/wrap.rs
+++ doomstack_derive/src/doom/wrap.rs
@@ -1,5 +1,9 @@
 use proc_macro2::Ident;
 
+/// A [`Setting`] prescribing the derivation of a wrapping constructor `constructor` for a group of
+/// fields (struct or enum variant).
+///
+/// [`Setting`]: crate::doom::Setting
 pub(crate) struct Wrap {
     pub constructor: Ident,
 }
