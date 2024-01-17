47f209fcfc68144f477a82576e612b54afe93fd3
Matteo Monti <matteo.monti@yahoo.it>
2024-01-17 16:49:54 +0100

    Begin documentation of `Derive::derive_struct`

--- doomstack_derive/src/doom/derive/derive_struct.rs
+++ doomstack_derive/src/doom/derive/derive_struct.rs
@@ -4,16 +4,27 @@ use quote::quote;
 use syn::Index;
 
 impl Derive {
+    /// Derives the `Doom` trait for a struct.
     pub(in crate::doom::derive) fn derive_struct_doom(
         identifier: &Ident,
         settings: &Settings,
         fields: &Fields,
     ) -> TokenStream {
+        // Derive `Doom::tag`
+
         let tag = quote! {
             fn tag(&self) -> &'static str {
                 stringify!(#identifier)
             }
         };
+
+        // Derive the struct's field binds, enabling the user to reference fields directly from
+        // `#[doom(description(...))]` or `#[doom(keep_original(...))]`:
+        //  - If the struct is named, a reference to each field is bound to a variable of the same
+        //    name.
+        //  - If the struct is unnamed, a reference to the `n`-th member is bound to a variable
+        //    named `_#n`.
+        //  - If the struct is a unit, no binds are necessary.
 
         let binds = match fields {
             Fields::Named(fields) => fields
@@ -23,19 +34,22 @@ impl Derive {
 
             Fields::Unnamed(types) => (0..types.len() as u32)
                 .map(|index| {
-                    let bind = Ident::new(format!("_{index}",).as_str(), Span::call_site());
+                    let identifier = Ident::new(format!("_{index}",).as_str(), Span::call_site());
 
                     let index = Index {
                         index,
                         span: Span::call_site(),
                     };
 
-                    quote!(let #bind = &self.#index;)
+                    quote!(let #identifier = &self.#index;)
                 })
                 .collect(),
 
             Fields::Unit => Vec::new(),
         };
+
+        // Derive description format: just the format literal if static,
+        // dynamically formatted with `format!` if owned
 
         let format = match &settings.description {
             Description::Static { description } => {
@@ -46,12 +60,19 @@ impl Derive {
             )),
         };
 
+        // Derive `Doom::description`
+
         let description = quote! {
             fn description(&self) -> doomstack::Description {
                 #(#binds)*
                 #format
             }
         };
+
+        // Derive (optional) `Doom::keep_original` if prescribed by `settings`
+
+        // Note: a default implementation of `Doom::keep_original` is provided, which
+        // always returns `false`.
 
         let keep_original = settings.keep_original.as_ref().map(|keep_original| {
             let condition = keep_original
@@ -67,6 +88,8 @@ impl Derive {
                 }
             }
         });
+
+        // Derive `Doom` trait
 
         quote! {
             impl doomstack::Doom for #identifier {
