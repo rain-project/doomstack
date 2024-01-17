10ce4af3cfac51557ce0aeba941e82164cdb6e10
Matteo Monti <matteo.monti@yahoo.it>
2024-01-17 12:34:49 +0100

    Document `Fields`

--- doomstack_derive/src/doom/fields.rs
+++ doomstack_derive/src/doom/fields.rs
@@ -1,6 +1,18 @@
 use proc_macro2::Ident;
 use syn::Type;
 
+/// The fields in a group of fields (struct or enum variant).
+///
+/// Can be: [`Named`] (storing each field's type and identifier); [`Unnamed`] (or tuple-like,
+/// storing each field's type); or [`Unit`] (no field at all).
+///
+/// Note: a [`Named`] / [`Unnamed`] group of fields that contains no fields is still [`Named`] /
+/// [`Unnamed`], not [`Unit`]. It is the type of bracket (`{}` for [`Named`], `()` for [`Unnamed`],
+/// absent for [`Unit`]) that determines the variant of [`Fields`].
+///
+/// [`Named`]: Fields::Named
+/// [`Unit`]: Fields::Unit
+/// [`Unnamed`]: Fields::Unnamed
 pub(crate) enum Fields {
     Named(Vec<(Type, Ident)>),
     Unnamed(Vec<Type>),
@@ -8,9 +20,12 @@ pub(crate) enum Fields {
 }
 
 impl Fields {
+    /// Parses a [`Fields`] from a [`syn::Fields`].
     pub fn parse(fields: &syn::Fields) -> Self {
         match fields {
             syn::Fields::Named(fields) => {
+                // Extract the type and identifier of each field
+
                 let fields = fields
                     .named
                     .iter()
@@ -21,6 +36,8 @@ impl Fields {
             }
 
             syn::Fields::Unnamed(fields) => {
+                // Extract the type of each field
+
                 let types = fields
                     .unnamed
                     .iter()
